980cffd405cd5acc71d4489516bb355086613f82
rockyboo3 <caitlyn.parrishlewis@gmail.com>
2025-04-10 20:23:25 -0400

    changed import method again in attempt to fix console error (legacy)

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+// import { initializeApp } from "firebase/app";
+// import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { initializeApp } from "http://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "http://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
