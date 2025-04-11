f51b67b1fbac7d242ea18adad94ec9e04f268e44
rockyboo3 <caitlyn.parrishlewis@gmail.com>
2025-04-10 21:57:51 -0400

    added working confetti demo

---
+++ confetti_demo/confetti.js
@@ -0,0 +1,13 @@
+const jsConfetti = new JSConfetti();
+
+jsConfetti.addConfetti({
+    emojis: ['💚'],
+    emojiSize: 60,
+    confettiNumber: 30,
+});
+
+// jsConfetti.addConfetti({
+//     emojis: ['❌'],
+//     emojiSize: 60,
+//     confettiNumber: 35,
+// });
